1606e7390cf7ee82ef192be440f11148ba78861e
Gerardo Rodriguez <55673748+g3rrarrd@users.noreply.github.com>
2024-04-17 16:18:41 -0600

    Delete src directory

--- src/main/java/com/spootify/backend_spootify/Repositories/Planes_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.spootify.backend_spootify.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spootify.backend_spootify.Models.Plan;
-
-public interface Planes_Repository extends JpaRepository<Plan, Integer>{
-    
-}
--- src/main/java/com/spootify/backend_spootify/Repositories/Tallas_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.spootify.backend_spootify.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spootify.backend_spootify.Models.Tallas;
-
-public interface Tallas_Repository extends JpaRepository<Tallas, Integer>{
-    
-}
